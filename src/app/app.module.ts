a3f8d6b7bc3cab8dd3802a8e084bd38018545814
Facundo Spinelli <facunehu@gmail.com>
2023-01-16 16:50:33 -0300

    Header armado e imagenes agregadas en assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PerfilComponent,
   ],
   imports: [
     BrowserModule,
